86329ebd116dce18cb0319c40101ab2b975fece4
santiago <elversantiago84@gmail.com>
2025-04-22 21:16:29 -0500

    start api

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  await app.listen(3000);
+}
+bootstrap();
