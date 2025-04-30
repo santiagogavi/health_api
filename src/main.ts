dca0b249cdc19d6f4e565f629f1327c84ce75778
santiago <elversantiago84@gmail.com>
2025-04-29 21:34:55 -0500

    new reques

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import {ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const configService = app.get(ConfigService); 
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 
-  app.enableCors();
-
-  await app.listen(3000);
 }
 bootstrap();
